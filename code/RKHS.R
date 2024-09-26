60f69dd13373e5267af04401f833b13c9d0307be
ShunichiroT <126551411+ShunichiroT@users.noreply.github.com>
2024-09-26 10:48:33 +1000

    Add files via upload

---
+++ code/RKHS.R
@@ -0,0 +1,93 @@
+library(BGLR)
+library(stringr)
+library(data.table)
+library(dplyr)
+library(iml)
+
+## Read data 
+## Assume that each data has n lines(rows) & m-1 markers + 1 phenotype at the end(columns)
+data_train <- fread("YOUR TRAIN DATA",header=TRUE)
+data_test <- fread("YOUR TEST DATA",header=TRUE)
+data <- rbind(data_train,data_test)
+
+## Preprocess the data to be processed by the model
+data_qtl <- data.frame(lapply(data[,1:(length(data[1,])-1)], as.numeric))
+data_pheno <- data[,length(data[1,]):length(data[1,])]
+
+D <- as.matrix(dist(data_qtl,method="euclidean"))^2
+D <- D/mean(D)
+h <- 1
+K <- exp(-h*D)
+
+y <- as.numeric(unlist(data_pheno))
+
+## Assign NA to the test data for evaluation
+train_id <- seq(1,nrow(data_train))
+test_id <- seq((nrow(data_train)+1),(nrow(data_train)+nrow(data_test)))
+
+y_test <- y
+y_test[test_id] <- NA
+
+## Predict train data phenotypes by the model
+nIter <- 12000
+burnIn <- 2000
+
+fm <- BGLR(y=y_test,ETA=list(list(K=K,model='RKHS')),
+           nIter=nIter,burnIn=burnIn,verbose=FALSE,saveAt='eig_')
+
+y_predicted <- fm$yHat[test_id]
+y_actual = y[test_id]
+
+## Calculate the metrics
+pearson <- cor(y_predicted, y_actual, method = c("pearson"))
+MSE <- mean((y_predicted - y_actual)^2)
+
+record <- data.frame(cbind(pearson,MSE))
+colnames(record) <- c('Pearson_r','MSE')
+
+## Store prediction result for the test data
+result_prediction_test <- cbind(y_predicted,y_actual)
+colnames(result_prediction_test) <- c('predicted','actual')
+
+## Store prediction result for the train data
+y_predicted_train <- fm$yHat[train_id]
+y_actual_train <- y[train_id]
+
+result_prediction_train <- cbind(y_predicted_train,y_actual_train)
+colnames(result_prediction_train) <- c('predicted','actual')
+
+## Extract marker effect
+true_model <- function(newdata) {
+  qtl <- rbind(data_qtl,newdata)
+  pred <- c(y_test,rep(NA, nrow(newdata)))
+  len_beg <- nrow(data_qtl)+ 1
+  len_end <- nrow(data_qtl)+ nrow(newdata)
+  
+  D <- as.matrix(dist(qtl,method="euclidean"))^2
+  D <- D/mean(D)
+  h <- 1
+  K <- exp(-h*D)
+
+  f <- BGLR(y=pred,ETA=list(list(K=K,model='RKHS')),
+             nIter=nIter,burnIn=burnIn,verbose=FALSE,saveAt='eig_')
+  
+  return(f[["yHat"]][len_beg:len_end])
+}
+
+predictor <- Predictor$new(NULL, data = data_qtl, y=fm[["yHat"]], predict.fun=true_model)
+
+effect <- data.frame()
+if(length(test_id) < 50){len <- length(test_id)}else{len <- 50}
+for(j in 1:len){
+  shapley <- Shapley$new(predictor, x.interest = data_qtl[test_id[j],], sample.size = 1)
+  tmp <- data.frame(t(shapley$results[,1:2]))
+  colnames(tmp) <- colnames(data)[1:ncol(data)-1]
+  effect <- dplyr::bind_rows(effect, tmp[2,])
+}
+colnames(effect) <- colnames(data)[1:ncol(data)-1]
+
+## Save all results
+write.csv(record, "METRIC DATA")
+write.csv(effect, "MARKER EFFECT DATA")
+write.csv(result_prediction_train, "PREDICTION RESULT FOR TRAIN DATA")
+write.csv(result_prediction_test, "PREDICTION RESULT FOR TEST DATA")
---
+++ code/SVR.py
@@ -0,0 +1,52 @@
+
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
+from scipy.stats import pearsonr
+import pandas as pd
+import shap
+
+#import os
+
+## Read data 
+## Assume that each data has n lines(rows) & m-1 markers + 1 phenotype at the end(columns)
+data_train = pd.read_csv('YOUR TRAIN DATA')
+data_test = pd.read_csv('YOUR TEST DATA')
+train_x, train_y = data_train.iloc[:,:-1], data_train.iloc[:,-1]
+test_x, test_y = data_test.iloc[:,:-1], data_test.iloc[:,-1]
+    
+## Train the model
+svr = SVR()
+svr.fit(train_x, train_y);
+     
+## Predict phenotypes for the test data
+predicted = svr.predict(test_x)
+
+## Calculate the metrics
+actual_test = test_y.values.tolist()
+mse = mean_squared_error(actual_test, predicted)
+r = pearsonr(actual_test, predicted)[0]
+
+## Store the metrics
+record = pd.DataFrame([r,mse]).T
+record.columns = ['Pearson_r','MSE']
+
+## Store prediction result for the test data
+result_prediction_test = pd.concat([pd.DataFrame(predicted),pd.DataFrame(actual_test)],axis=1)
+result_prediction_test.columns = ['predicted','actual']         
+         
+## Store prediction result for the train data
+predicted_train = svr.predict(train_x)
+actual_train = train_y.values.tolist()
+result_prediction_train = pd.concat([pd.DataFrame(predicted_train),pd.DataFrame(actual_train)],axis=1)
+result_prediction_train.columns = ['predicted','actual']   
+
+## Extract marker effect
+explainer = shap.KernelExplainer(svr.predict,shap.sample(train_x, 50))
+effect = pd.DataFrame(abs(explainer.shap_values(test_x)).sum(axis=0)).T
+effect.columns = list(data_train.columns)[:-1]
+
+## Save all results
+record.to_csv('METRIC DATA')
+effect.to_csv('MARKER EFFECT DATA')
+result_prediction_train.to_csv('PREDICTION RESULT FOR TRAIN DATA')
+result_prediction_test.to_csv('PREDICTION RESULT FOR TEST DATA')
